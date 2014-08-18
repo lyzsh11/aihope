6dff489f36409770fb3a08456f11797ced30657c
lyzsh11 <lyzsh11@126.com>
2014-08-19 01:11:09 +0800

    添加关于我们页

---
+++ about.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>关于我们</title>
+<link href="css/css_detail.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body style="margin:0 0">
+<iframe src="header.html" scrolling="no" height="130px" width="100%" frameborder="0"></iframe>
+<div class="box_in">
+	<div class="left">
+		<iframe src="left.html" scrolling="no" height="600px" width="100%" frameborder="0"></iframe>
+	</div>
+    <div class="right">
+    	<div class="text_head">关于我们</div>
+        <div style="background:url(image/bar_long.png) repeat-x; height:5px; margin-top:5px"></div>
+        <div class="text_content_big">
+        	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我们——爱心帮，是一个做公益项目的企业。爱心帮教的目的是通过网友给教师提供保障金的方法，来鼓励和支持教师为乡村孩子、农民工子弟和教育欠发达区域的少年儿童提供优质的教学。您可以定向捐赠某位感动您的老师，也可以选择自动分配老师（爱心帮将根据大家奖赠的情况去尽量合理地分配给每位申请的老师）。爱心帮不会抽取或扣留一分钱，但此款项的到账可能比直接定向捐给老师略有延迟。<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;所有账目出入均可查询。<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="link" href="http://cmbchina.com/">招商银行</a><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一卡通卡号: 6214 8501 0331 8306<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;开户地: 北京 
+<a class="link" href="https://pbnj.ebank.cmbchina.com/CmbBank_GenShell/UI/GenShellPC/Login/Login.aspx">网上银行大众版</a><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;查询密码: 140812
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
